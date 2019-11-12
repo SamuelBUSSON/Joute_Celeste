569e0d5d3f58537e8a8a0f735b0621c1620e98c4
Samuel Busson <samuel.busson@hotmail.fr>
2019-11-12 22:45:52 +0100

    Initial commit

---
+++ Assets/Scripts/Displacement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Displacement : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    private float h;
+    private float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            h = Input.GetAxis("Horizontal");
+        }
+        else
+        {
+            h = 0;
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            v = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            v = 0;
+        }
+
+        Vector3 displaceVector = new Vector3(h, v, 0).normalized;
+
+        transform.position += displaceVector * speed;
+
+    }
+}
