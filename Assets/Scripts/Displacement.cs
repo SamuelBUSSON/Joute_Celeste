e12ebfc3b3539d331c8f9151f6260a2b0c14034d
Samuel Busson <samuel.busson@hotmail.fr>
2019-11-12 23:47:18 +0100

    First proto

--- Assets/Scripts/Displacement.cs
+++ Assets/Scripts/Displacement.cs
@@ -19,9 +19,6 @@ public class Displacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        
-
         if (Input.GetAxis("Horizontal") != 0)
         {
             h = Input.GetAxis("Horizontal");
---
+++ Assets/Scripts/ObjectSpawn.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    public GameObject starPrefab;
+
+    public float distance = 1.0f;
+    public float coolDown = 0.2f;
+
+    public float launchStrength = 2.0f;
+
+    private Transform currentChild;
+    private float angle = 0.0f;
+    private Vector3 displaceAngleVector;
+
+    private float coolDownTimer = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displaceAngleVector = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.childCount != 1)
+        {
+            currentChild = Instantiate(starPrefab, transform.position, Quaternion.identity).transform;
+
+            currentChild.SetParent(transform);
+
+            currentChild.transform.position = transform.position + displaceAngleVector;
+        }
+        else
+        {
+            float x = Input.GetAxis("ControllerVertical");
+            float y = Input.GetAxis("ControllerHorizontal");
+            if (x != 0.0f || y != 0.0f)
+            {
+                angle = Mathf.Atan2(y, x);
+                Debug.Log(angle);
+                
+                displaceAngleVector.x = distance * Mathf.Sin(angle);
+                displaceAngleVector.y = distance * -Mathf.Cos(angle);
+
+                currentChild.transform.position = transform.position + displaceAngleVector;
+            }
+            if(coolDownTimer >= coolDown)
+            {
+                if (Input.GetButton("Attack1"))
+                {
+                    currentChild.GetComponent<TrailRenderer>().enabled = true;
+                    coolDownTimer = 0.0f;
+                    currentChild.SetParent(null);
+                    Vector3 heading = currentChild.transform.position - transform.position;
+                    currentChild.GetComponent<Rigidbody>().velocity = heading * launchStrength;
+                }
+            }
+            
+
+            coolDownTimer += Time.deltaTime;
+        }
+        
+    }
+}
