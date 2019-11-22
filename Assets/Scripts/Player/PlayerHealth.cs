ff7f5b7a77bdab54f07df7a840d32377dcb50c80
Santoro Fabrizio <fabrizio1996@live.fr>
2019-11-22 09:00:44 +0100

    Projectile have mass, type and deal damage

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Serializable]
+    public struct SHealthThreshold //TODO: maybe add a range to enlarge the entity
+    {
+        public int health;
+    }
+
+    public List<SHealthThreshold> thresholds;
+    
+    public int Health;
+
+    private int indexThreshold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+
+        if (Health >= 0)
+        {
+            if (thresholds[indexThreshold].health >= Health)
+            {
+                
+            }
+        }
+        else
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //TODO
+    }
+}
---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum EProjectileType
+{
+    ASTEROID = 0,
+    PLANET,
+    STAR
+}
+
+public class Projectile : MonoBehaviour
+{
+    public EProjectileType type;
+    public int playerIndex;
+    public int damage;
+
+    private void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Projectile proj = other.transform.GetComponent<Projectile>();
+
+        if (proj)
+        {
+            if(proj.type > type)
+                Destroy(gameObject); //TODO: add the kaboom
+            else if(proj.type < type)
+            {
+                //TODO: add the kaboom
+                Destroy(proj.gameObject);
+            }
+            else
+            {
+                //TODO: add the kaboom
+                Destroy(proj.gameObject);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            PlayerHealth player = other.transform.GetComponent<PlayerHealth>();
+
+            //TODO: check if it's the creator
+            if (player)
+            {
+                player.TakeDamage(damage);
+            }
+            
+           
+        }
+    }
+}
