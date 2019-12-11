27e6dfc50ac55ed1402c59cf3c94c2603126b391
Fabrizio <fabrizio1996@live.fr>
2019-12-11 17:56:02 +0100

    Starting AI

---
+++ Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityTemplateProjects.AI
+{
+    public class AIController : MonoBehaviour
+    {
+        public AIState state;
+
+        private void Start()
+        {
+            state = new AIStateLookForProj();
+            
+            state.OnStart();
+        }
+
+        private void Update()
+        {
+            state.OnUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/AIState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects.AI
+{
+
+    public abstract class AIState : MonoBehaviour
+    {
+        public AIController controller;
+        
+        public abstract void OnStart();
+        public abstract void OnUpdate();
+    }
+}
