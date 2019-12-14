98eb2315aca298a779a865e140a39de3afa9f001
Fabpk90 <fabrizio1996@live.fr>
2019-12-14 18:12:07 +0100

    Adde parallax to main menu

--- Assets/Scripts/Util/GoBackToMenu.cs
+++ Assets/Scripts/Util/GoBackToMenu.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 namespace UnityTemplateProjects.Util
 {
     public class GoBackToMenu : MonoBehaviour
     {
-        
+        private void Update()
+        {
+            if (Gamepad.current.bButton.wasPressedThisFrame)
+                SceneManager.LoadScene(0);
+        }
     }
 }
